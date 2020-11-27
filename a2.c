f6c2c10103df61f7baccca3e2111d5b2d11c3c94
Killer Lorenz <killer@student.tugraz.at>
2020-11-27 10:29:18 +0100

    Kommentare Kommentiershow

--- a2.c
+++ a2.c
@@ -1,15 +1,15 @@
 //-----------------------------------------------------------------------------
 // a2.c
 //
-// TODO
+// Playfair Cipher
+// A program which uses a simple playfair-like cipher alogirthm 
+// to encrypt and decrpyt messages using a choosable key
 //
 // Group: 12
 //
 // Author: 12007661
 //-----------------------------------------------------------------------------
 //
-
-//TODO mehrere definitionen in einer zeile????
 
 // Includes
 #include  <stdio.h>
@@ -22,9 +22,7 @@
 #define CHOICE_DECRYPT '2'
 
 #define MAX_INPUT_LENGTH 50
-
 #define FILL_CHAR 'X'
-
 #define SUCCESS 1
 
 // Forward definitions
@@ -33,12 +31,13 @@ void toUpper(char *text);
 void replaceLetters(char *text, char original, char new_char);
 
 char getUserChoice();
-void cipher(char* playfairSquare, OperationType op);
-void getInput(char* input, OperationType op);
-int createBigrams(char* input, OperationType op);
-void printBigram(char* bigram);
-void getIndices(char* playfairSquare, char element, char* element_x, char* element_y);
+void getInput(char* input, OperationType operation_type);
+void cipher(char* playfair_square, char* input, OperationType operation_type);
+int createBigrams(char* input, OperationType operation_type);
+void printOutput(char* message, OperationType operation_type);
+void printBigrams(char* bigrams);
 
+void getIndices(char* playfair_square, char element, char* element_x, char* element_y);
 char shiftRight(char index);
 char shiftLeft(char index);
 
@@ -46,38 +45,50 @@ char shiftLeft(char index);
 ///
 /// The main program
 /// 
-/// TODO
+/// Generates a playfair square based on a key that the user must input
+/// Then gives the user the choice to encrypt or decrypt a message
+/// The program only ends if the user wishes so
 ///
 /// @return always zero
 //
 int main()
 {
-  char playfairSquare[5][5];
-  generatePlayfairSquare(*playfairSquare);
+  char playfair_square[5][5];
+  generatePlayfairSquare(*playfair_square);
 
   char choice = 1;
   while(choice != CHOICE_STOP)
   {
     choice = getUserChoice();
+    OperationType operation_type;
 
     if (choice == CHOICE_ENCRYPT)
     {
-      cipher(*playfairSquare, IS_ENCRYPTING);
+      operation_type = IS_ENCRYPTING;
     }
     else if (choice == CHOICE_DECRYPT)
     {
-      cipher(*playfairSquare, IS_DECRYPTING);
+      operation_type = IS_DECRYPTING;
+    }
+
+    if (choice != CHOICE_INVALID && choice != CHOICE_STOP)
+    {
+    char input[MAX_INPUT_LENGTH] = "";
+
+    getInput(input, operation_type);
+    cipher(*playfair_square, input, operation_type);
+    printOutput(input, operation_type);
     }
   }
   return 0;
 }
 
-
 //-----------------------------------------------------------------------------
-/// Reads a character from stdin and returns it
-/// 
+/// Prompts the user to make a choice between 3 options
+/// Reads the input and only accepts values '0', '1' and '2'
+/// Everything else will automatically be set to the INVALID_CHOICE constant
 ///
-/// @return character from user input or INVALID_CHOICE if input has multiple characters
+/// @return a character between '0' and '2' or the INVALID_CHOICE constant
 //
 char getUserChoice() 
 {
@@ -88,14 +99,16 @@ char getUserChoice()
     "0 - Programm beenden\n\n"
   );
 
-  char choice, tmp = CHOICE_INVALID;
+  char choice;
+  char tmp = CHOICE_INVALID;
   int i = 0;
   do  
   {
     choice = tmp;
     tmp = getchar();
     i++;
-  } while (tmp != '\n');
+  } 
+  while (tmp != '\n');
 
   if (i > 2)
   {
@@ -106,30 +119,31 @@ char getUserChoice()
 }
 
 //-----------------------------------------------------------------------------
-/// TODO
+/// Encrypts or Decrypts a message based on a playfair square
 /// 
-///
-/// @return pointer to the char array
+/// @param playfair_square two dimensional array for ciphering
+/// @param input the message to en- or decrypt, already in bigram-form
+/// @param operation_type either IS_ENCTYPTING or IS_DECRYPTING
 //
-void cipher(char* playfairSquare, OperationType op)
-{
-  char input[MAX_INPUT_LENGTH] = "";
-  getInput(input, op);
-  
+void cipher(char* playfair_square, char* input, OperationType operation_type)
+{  
   int string_len = stringLength(input);
   for (int i = 0 ; i < string_len; i++){
 
     if (i % 2 == 1){
       char first = input[i - 1];
       char second = input[i];
-      char first_x, first_y, second_x, second_y;
+      char first_x; 
+      char first_y; 
+      char second_x; 
+      char second_y;
 
-      getIndices(playfairSquare, first, &first_x, &first_y);
-      getIndices(playfairSquare, second, &second_x, &second_y);
+      getIndices(playfair_square, first, &first_x, &first_y);
+      getIndices(playfair_square, second, &second_x, &second_y);
 
       if (first_y == second_y)
       {
-        if (op == IS_ENCRYPTING)
+        if (operation_type == IS_ENCRYPTING)
         {
           first_x = shiftRight(first_x);
           second_x = shiftRight(second_x);
@@ -142,7 +156,7 @@ void cipher(char* playfairSquare, OperationType op)
       }
       else if (first_x == second_x)
       {
-        if (op == IS_ENCRYPTING)
+        if (operation_type == IS_ENCRYPTING)
         {
           first_y = shiftRight(first_y);
           second_y = shiftRight(second_y);
@@ -160,54 +174,63 @@ void cipher(char* playfairSquare, OperationType op)
         second_x = temp;
       }
   
-      input[i - 1] = playfairSquare[first_y * SQUARE_SIDE + first_x];
-      input[i] = playfairSquare[second_y * SQUARE_SIDE + second_x];
+      input[i - 1] = playfair_square[first_y * SQUARE_SIDE + first_x];
+      input[i] = playfair_square[second_y * SQUARE_SIDE + second_x];
     }
   }
-
-  if (op == IS_ENCRYPTING) printf("Ciphertext: ");
-  else printf("Klartext: ");
-  printBigram(input);
 }
 
 //-----------------------------------------------------------------------------
-/// TODO
+/// Prompts the user for an input for en- or decrypting, 
+/// checks if it is valid and processes it into bigrams
+/// If the input is invalid it prompts the user again
 /// 
-///
-/// @return pointer to the char array
+/// @param input the array to save the input in
+/// @param operation_type either IS_ENCTYPTING or IS_DECRYPTING 
 //
-void getInput(char* input, OperationType op)
+void getInput(char* input, OperationType operation_type)
 {
   // Copied code from: Framework
   // START
   while(1) 
   {
-    if (op == IS_ENCRYPTING) printf("Klartext: ");
+    if (operation_type == IS_ENCRYPTING) printf("Klartext: ");
     else printf("Ciphertext: ");
 
     fgets(input, MAX_INPUT_LENGTH, stdin);
 
-    if(checkStringValidity(input, MAX_INPUT_LENGTH, op)) break;
+    if(checkStringValidity(input, MAX_INPUT_LENGTH, operation_type)) break;
   }
   // END
 
-  cleanString(input, op);
-  if (createBigrams(input, op) == ERROR) getInput(input, op);
+  cleanString(input, operation_type);
+  if (createBigrams(input, operation_type) == ERROR)
+  {
+    getInput(input, operation_type);
+  }
 
-  if (op == IS_ENCRYPTING) printf("Vorbereiteter Klartext: ");
-  else printf("Vorbereiteter Ciphertext: ");
+  if (operation_type == IS_ENCRYPTING)
+  {
+    printf("Vorbereiteter Klartext: ");
+  }
+  else
+  {
+    printf("Vorbereiteter Ciphertext: ");
+  } 
 
-  printBigram(input);
+  printBigrams(input);
 }
 
 //-----------------------------------------------------------------------------
-/// TODO
+/// Takes a message for en- or decrypting and processes it into bigrams
+/// If the message is for decrypting, also checks if the message is valid
 ///
-/// @param text string to check 
+/// @param input the message to process into bigrams
+/// @param operation_type either IS_ENCTYPTING or IS_DECRYPTING
 ///
-/// @return length of the string
+/// @return SUCCESS if the message was valid ERROR otherwise
 //
-int createBigrams(char* input, OperationType op)
+int createBigrams(char* input, OperationType operation_type)
 {
   int string_len = stringLength(input);
   char last_char = 0;
@@ -220,7 +243,7 @@ int createBigrams(char* input, OperationType op)
 
     if (i % 2 == 1 && last_char == current_char)
     {
-      if (op == IS_DECRYPTING) {
+      if (operation_type == IS_DECRYPTING) {
         return ERROR;
       }
       bigram[j++] = FILL_CHAR;
@@ -238,12 +261,15 @@ int createBigrams(char* input, OperationType op)
 
   if (string_len % 2 != 0)
   {
-    if (op == IS_ENCRYPTING) 
+    if (operation_type == IS_ENCRYPTING) 
     {
       bigram[string_len] = FILL_CHAR;
       string_len++;
     }
-    else return ERROR;
+    else
+    {
+      return ERROR;
+    }
   }
 
   for (int i = 0; i < string_len; i++) input[i] = bigram[i];
@@ -252,70 +278,96 @@ int createBigrams(char* input, OperationType op)
 }
 
 //-----------------------------------------------------------------------------
-/// TODO
+/// Prints a variable amount if bigrams to stdout
 ///
-/// @param text string to check 
-///
-/// @return length of the string
+/// @param bigrams the bigrams to print
 //
-void printBigram(char* bigram)
+void printBigrams(char* bigrams)
 {
-  int string_len = stringLength(bigram);
+  int string_len = stringLength(bigrams);
 
   for (int i = 0; i < string_len; i++)
   {
-    if ((i % 2) == 1 && i != string_len - 1) printf("%c ", bigram[i]);
-    else printf("%c", bigram[i]);
+    printf("%c", bigrams[i]);
+    if ((i % 2) == 1 && i != string_len - 1)
+    {
+      printf(" ");
+    }
   }
-
   printf("\n");
 }
 
+//-----------------------------------------------------------------------------
+/// Prints the output after en- or decrypting to stdout
+///
+/// @param message the proccessed message to output
+//
+void printOutput(char* message, OperationType operation_type)
+{
+  if (operation_type == IS_ENCRYPTING)
+  {
+    printf("Ciphertext: ");
+  }
+  else
+  {
+    printf("Klartext: ");
+  } 
+  printBigrams(message);
+}
 
 //-----------------------------------------------------------------------------
-/// TODO
+/// Shifts an index one to the right/down and returns it
+/// If the index is already in the right most/lowest positon of the playfair square
+/// it gets set to the left most/highest positon instead
 ///
-/// @param text string to check 
+/// @param index the index to shift
 ///
-/// @return length of the string
+/// @return the new index
 //
 char shiftRight(char index)
 {
   char new_index = index + 1;
-  if (new_index == SQUARE_SIDE) new_index = 0;
-
+  if (new_index == SQUARE_SIDE)
+  {
+    new_index = 0;
+  } 
   return new_index;
 }
 
 //-----------------------------------------------------------------------------
-/// TODO
+/// Shifts an index one to the left/up and returns it
+/// If the index is already in the left most/highest positon of the playfair square
+/// it gets set to the right most/lowest positon instead
 ///
-/// @param text string to check 
+/// @param index the index to shift
 ///
-/// @return length of the string
+/// @return the new index
 //
 char shiftLeft(char index)
 {
   char new_index = index - 1;
-  if (new_index == -1) new_index = SQUARE_SIDE - 1;
-
+  if (new_index == -1)
+  {
+    new_index = SQUARE_SIDE - 1;
+  } 
   return new_index;
 }
 
 //-----------------------------------------------------------------------------
-/// TODO
+/// Looks for the indices of a character in a 2D-array of characters
 ///
-/// @param text string to check 
-///
-/// @return length of the string
+/// @param playfair_square the 2D-array to search in
+/// @param element the char to search for
+/// @param element_x gets set to the x index of the character
+/// @param element_y gets set to the y index of the character
 //
-void getIndices(char* playfairSquare, char element, char* element_x, char* element_y){
+void getIndices(char* playfair_square, char element, char* element_x, char* element_y){
 
   for (int y_index = 0; y_index < SQUARE_SIDE; y_index++)
   {
     for (int x_index = 0; x_index < SQUARE_SIDE; x_index++)
     {
-      if (playfairSquare[y_index * SQUARE_SIDE + x_index] == element)
+      if (playfair_square[y_index * SQUARE_SIDE + x_index] == element)
       {
         *element_x = x_index;
         *element_y = y_index;
